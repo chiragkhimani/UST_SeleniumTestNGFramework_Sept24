8c48d2dd10687b02f269e26a2e15b9a26ca96d79
chiragkhimani <khimanichirag@gmail.com>
2024-10-08 15:53:59 +0530

    data driven test

--- src/test/java/com/automation/test/LoginLogoutTest.java
+++ src/test/java/com/automation/test/LoginLogoutTest.java
@@ -1,9 +1,12 @@
 package com.automation.test;
 
 import com.automation.utils.ConfigReader;
+import com.automation.utils.ExcelUtils;
 import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+
+import java.util.List;
 
 public class LoginLogoutTest extends BaseTest {
 
@@ -16,15 +19,18 @@ public class LoginLogoutTest extends BaseTest {
 
     @DataProvider
     public Object[][] getValidCredentials() {
-        Object[][] credentials = {
-                {"standard_user", "secret_sauce"},
-                {"problem_user", "secret_sauce"},
-                {"performance_glitch_user", "secret_sauce"},
-                {"error_user", "secret_sauce"},
-                {"visual_user", "secret_sauce"}
-        };
+        ExcelUtils excelUtils = new ExcelUtils("Credentials.xlsx", "ValidData");
+        List<List<String>> tableData = excelUtils.getData();
+        Object[][] data = new Object[tableData.size()][tableData.get(0).size()];
 
-        return credentials;
+        for (int i = 0; i < tableData.size(); i++) {
+            List<String> row = tableData.get(i);
+            for (int j = 0; j < row.size(); j++) {
+                data[i][j] = tableData.get(i).get(j);
+            }
+        }
+
+        return data;
     }
 
     @Test
--- src/test/java/com/automation/utils/ExcelUtils.java
+++ src/test/java/com/automation/utils/ExcelUtils.java
@@ -1,22 +1,51 @@
 package com.automation.utils;
 
+import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class ExcelUtils {
-    public static void main(String[] args) throws IOException {
-        XSSFWorkbook workbook = new XSSFWorkbook("src/test/resources/data/Credentials.xlsx");
-        XSSFSheet sheet = workbook.getSheet("ValidData");
 
-        for (int i = 0; i < sheet.getLastRowNum(); i++) {
-            XSSFRow row = sheet.getRow(i);
-            for (int j = 0; j < row.getLastCellNum(); j++) {
-                System.out.print(row.getCell(j).getStringCellValue() + " ");
-            }
-            System.out.println();
+    XSSFWorkbook workbook;
+    XSSFSheet sheet;
+
+    public ExcelUtils(String fileName, String sheetName) {
+        try {
+            workbook = new XSSFWorkbook("src/test/resources/data/" + fileName);
+            sheet = workbook.getSheet(sheetName);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
+
+    public int getTotalNumOfRow() {
+        return sheet.getLastRowNum();
+    }
+
+    public int getTotalNumberOfCol() {
+        return sheet.getRow(0).getLastCellNum();
+    }
+
+    public List<List<String>> getData() {
+        List<List<String>> tableData = new ArrayList<>();
+
+        for (int i = 1; i < getTotalNumOfRow(); i++) {
+            XSSFRow row = sheet.getRow(i);
+            List<String> rowData = new ArrayList<>();
+            for (int j = 0; j < row.getLastCellNum(); j++) {
+                rowData.add(row.getCell(j).getStringCellValue());
+            }
+            tableData.add(rowData);
+        }
+
+        return tableData;
+    }
+
+
 }
